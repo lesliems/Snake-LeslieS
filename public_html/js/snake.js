71372eb9aefcae4cd75fce816acf694e60f15881
leslie Segovia <lesliemsegovia@gmail.com>
2014-10-06 10:46:09 -0700

    2nd commit

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,3 +5,14 @@ var context;
 var screenWidth;
 
 var screenHeight;
+
+function gameInitialize(){
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+}
+function gameLoop(){
+    
+}
+function gameDraw(){
+    
+}
