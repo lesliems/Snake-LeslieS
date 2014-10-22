f0d508a2b6abe2348711879576816c7c1e8c8b3d
leslie Segovia <lesliemsegovia@gmail.com>
2014-10-22 09:15:03 -0700

    new commit

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -101,16 +101,27 @@ function foodInitialize(){
 }
 function foodDraw(){
     context.fillStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
 function setFoodPosition(){
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor (randomX / snakeSize);
+    food.y = Math.floor (randomY / snakeSize);
 }
+/*------------------------------------------------------------------------------
+ * Input Functions
+ * -----------------------------------------------------------------------------
+ */
 
 function keyboardHandler(event){
     console.log(event);
+       
+    if(event.keyCode == "39" && snakeDirection != "left"){
+    snakeDirection = "right";
+    }
+    else if (event.keyCode =="40"){
+        snakeDirection = "down";
+    }
 }
