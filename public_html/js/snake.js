2016296584d114c139f0669f2e83d79c74bb4719
leslie Segovia <lesliemsegovia@gmail.com>
2014-10-27 10:46:49 -0700

    finish videos

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -13,6 +13,8 @@ var context;
 var screenWidth;
 var screenHeight;
 
+var gameState;
+
 /* -----------------------------------------------------------------------------
  * Executing Game Code
  * -----------------------------------------------------------------------------
@@ -20,76 +22,81 @@ var screenHeight;
 gameInitialize();
 snakeInitialize();
 foodInitialize();
-setInterval(gameLoop, 1000/30);
+setInterval(gameLoop, 1000 / 30);
 
 /* -----------------------------------------------------------------------------
  * Game Functions
  * -----------------------------------------------------------------------------
  */
 
-function gameInitialize(){
+function gameInitialize() {
     var canvas = document.getElementById("game-screen");
     context = canvas.getContext("2d");
-   
+
     screenWidth = window.innerWidth;
     screenHeight = window.innerHeight;
-    
+
     canvas.width = screenWidth;
     canvas.height = screenHeight;
-    
+
     document.addEventListener("keydown", keyboardHandler);
+    
+    setState("PLAY");
 }
-function gameLoop(){
+function gameLoop() {
     gameDraw();
-    snakeUpdate();
-    snakeDraw();
-    foodDraw();
+    if (gameState == "PLAY") {
+        snakeUpdate();
+        snakeDraw();
+        foodDraw();
+    }
 }
-function gameDraw(){
+function gameDraw() {
 //    context.fillStyle = "url('http://p1.pichost.me/640/48/1712836.jpg')";
 ////     context.fillStyle = "rgb(23, 201, 255)";
 //     context.fillRect(0, 0, screenWidth, screenHeight);
+    context.clearRect(0, 0, screenWidth, screenHeight);
 }
 /* -----------------------------------------------------------------------------
  * Snake Functions
  * ----------------------------------------------------------------------------- 
  */
-function snakeInitialize(){
+function snakeInitialize() {
     snake = [];
     snakeLength = 3;
     snakeSize = 20;
     snakeDirection = "down";
-    
-    for(var index = snakeLength - 1; index >= 0; index--){
-        snake.push( {
-            x: index, 
+
+    for (var index = snakeLength - 1; index >= 0; index--) {
+        snake.push({
+            x: index,
             y: 0
         });
     }
 }
-function snakeDraw(){
-    for(var index = 0; index < snake.length; index++){
+function snakeDraw() {
+    for (var index = 0; index < snake.length; index++) {
         context.fillStyle = "white";
         context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
     }
 }
-function snakeUpdate(){
+function snakeUpdate() {
     var snakeHeadX = snake[0].x;
     var snakeHeadY = snake[0].y;
-    
-    if(snakeDirection == "down"){
+
+    if (snakeDirection == "down") {
         snakeHeadY++;
     }
-    else if(snakeDirection == "right") {
+    else if (snakeDirection == "right") {
         snakeHeadX++;
     }
-    else if(snakeDirection == "up"){
+    else if (snakeDirection == "up") {
         snakeHeadY--;
     }
-    else if(snakeDirection == "left") {
+    else if (snakeDirection == "left") {
         snakeHeadX--;
     }
-   
+
     checkFoodCollisions(snakeHeadX, snakeHeadY);
     checkWallCollisions(snakeHeadX, snakeHeadY);
 
@@ -102,61 +109,78 @@ function snakeUpdate(){
  * Food Functions
  * -----------------------------------------------------------------------------
  */
-function foodInitialize(){
-    food ={
+function foodInitialize() {
+    food = {
         x: 0,
         y: 0
     };
     setFoodPosition();
 }
-function foodDraw(){
+function foodDraw() {
     context.fillStyle = "white";
     context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize, snakeSize);
 }
-function setFoodPosition(){
+function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
-    
-    food.x = Math.floor (randomX / snakeSize);
-    food.y = Math.floor (randomY / snakeSize);
+
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
 /*------------------------------------------------------------------------------
  * Input Functions
  * -----------------------------------------------------------------------------
  */
 
-function keyboardHandler(event){
+function keyboardHandler(event) {
     console.log(event);
-       
-    if(event.keyCode == "39" && snakeDirection != "left"){
-    snakeDirection = "right";
+
+    if (event.keyCode == "39" && snakeDirection != "left") {
+        snakeDirection = "right";
     }
-    else if (event.keyCode =="40" && snakeDirection != "up"){
+    else if (event.keyCode == "40" && snakeDirection != "up") {
         snakeDirection = "down";
     }
-    else if (event.keyCode =="37" && snakeDirection !="right"){
+    else if (event.keyCode == "37" && snakeDirection != "right") {
         snakeDirection = "left";
     }
-    else if (event.keyCode == "38" && snakeDirection !="down"){
+    else if (event.keyCode == "38" && snakeDirection != "down") {
         snakeDirection = "up";
     }
-    }
-function checkFoodCollisions(snakeHeadX, snakeHeadY){
-    if(snakeHeadX == food.x && snakeHeadY == food.y){
+}
+function checkFoodCollisions(snakeHeadX, snakeHeadY) {
+    if (snakeHeadX == food.x && snakeHeadY == food.y) {
         snake.push({
             x: 0,
             y: 0
         });
         snakeLength++;
-        
+
         var randomX = Math.floor(Math.random() * screenWidth);
         var randomY = Math.floor(Math.random() * screenHeight);
-        
-        food.x = Math.floor (randomX / snakeSize);
-        food.y = Math.floor (randomY / snakeSize);
+
+        food.x = Math.floor(randomX / snakeSize);
+        food.y = Math.floor(randomY / snakeSize);
     }
 }
-function checkWallCollisions(snakeHeadX, snakeHeadY){
+function checkWallCollisions(snakeHeadX, snakeHeadY) {
     snakeHeadX * snakeSize >= screenWidth || snakeHeadX * snakeSize < 0;
-    console.log("Wall Collision");
+}
+function initTexture(src) {
+    texture = gl.createTexture();
+    texture.image = new Image();
+    texture.image.onload = function() {
+        handleLoadedTexture(texture)
+    }
+
+    texture.image.src = src;
+}
+
+/*
+ * 
+ * 
+ * 
+ */
+function setState(state) {
+    gameState = state;
 }
